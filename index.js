5372f2e3945ee0a1121790a688b41279a5d1390b
swapan-miah <swaponmiah120@gmail.com>
2024-09-16 14:40:52 +0600

    purchase history add purchase date

--- index.js
+++ index.js
@@ -674,15 +674,15 @@ async function run() {
 
           const currentDate = new Date();
           const dateOnly = currentDate.toISOString().split("T")[0];
-          orderRecord.storeDate = dateOnly;
 
           // Insert into purchase history
           const purchase_info = {
             product_name: req.body.product_name,
             company_name: req.body.company_name,
             size: req.body.size,
-            store_quantity: req.body.quantity,
+            quantity: req.body.quantity,
             purchase_price: req.body.purchase_price,
+            storeDate: dateOnly,
           };
           const purchaseHistoryItem =
             await purchase_history_Collection.insertOne(purchase_info);
